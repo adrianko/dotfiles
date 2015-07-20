06ac4c24944d21c722fba824fe7ffd70eae3f990
Adrian Ko <adrianchko@gmail.com>
2015-07-20 14:30:42 +0100

    Remove touch.sh

---
+++ install.sh
@@ -0,0 +1,5 @@
+cp .vimrc ~/.vimrc
+cp .bash_profile ~/.bash_profile
+mkdir ~/.bash
+cd ~/.bash
+git clone git://github.com/jimeh/git-aware-prompt.git
--- touch.sh
+++
@@ -1,5 +0,0 @@
-cp .vimrc ~/.vimrc
-cp .bash_profile ~/.bash_profile
-mkdir ~/.bash
-cd ~/.bash
-git clone git://github.com/jimeh/git-aware-prompt.git
