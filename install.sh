613943e8f9a56fc1d565be7366dcccb160a22ba9
Adrian Ko <adrianchko@gmail.com>
2015-07-20 14:37:48 +0100

    Create .vim/syntax folders if not exists

--- install.sh
+++ install.sh
@@ -3,3 +3,10 @@ cp .bash_profile ~/.bash_profile
 mkdir ~/.bash
 cd ~/.bash
 git clone git://github.com/jimeh/git-aware-prompt.git
+if [ ! -d ~/.vim ]; then
+    mkdir ~/.vim
+fi
+
+if [ ! -d ~/.vim/syntax ]; then
+    mkdir ~/.vim/syntax
+fi
