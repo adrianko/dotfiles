84dffeb73ea8ec2c7a51380616838129b66d9cdc
Adrian Ko <adrianchko@gmail.com>
2015-07-22 08:51:23 +0100

    May as well install vim using brew

--- install.sh
+++ install.sh
@@ -15,7 +15,7 @@ fi
 
 if [ "$(uname)" == "Darwin" ]; then
     ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-    brew install scala go ncdu john nmap wget gradle hr
+    brew install scala go ncdu john nmap wget gradle hr vim
     brew tap caskroom/cask
     brew install brew-cask
     brew cask install java
